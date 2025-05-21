fdf9f3782b9054dea4a246f260202c9468232fa7
sakunakavishka <sakunakavishka1@gmail.com>
2025-05-21 23:26:18 +0530

    category_delete_using_name

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -36,3 +36,35 @@ export function createCategory(req,res){
         }
     )
 }
+export function deleteCategory(req,res){
+    if (req.user==null){
+        res.status(401).json({
+            message:"unauthorized"
+        })
+        return
+        }
+        if (req.user.type !=="admin"){
+            res.status(403).json({
+                message:"forbidden"
+            })
+            return
+        }
+        const name =req.params.name
+        Category.findOneAndDelete({name:name}).then(
+            ()=>{
+                res.json(
+                    {
+                    message:"Category delete successfully"
+                    }
+                 )
+             }
+        ).catch(
+            ()=>{
+                res.json(
+                    {
+                        message:"Category deletion failed"
+                    }
+                )
+            }
+        )
+    }
--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,7 +1,9 @@
 import express from "express";
-import {createCategory} from "../controllers/categoryController.js";
+import {createCategory, deleteCategory} from "../controllers/categoryController.js";
 const categoryRouter = express.Router();
 
 categoryRouter.post("/",createCategory)
+
+categoryRouter.delete("/:name",deleteCategory)
 
 export default categoryRouter;
